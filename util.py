230a72f285eb719fedf5ee7a0e778db5d28ac941
Simon Burton <simon@arrowtheory.com>
2017-02-16 23:29:41 +1100

    whack whack

---
+++ util.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+def all_subsets(n):
+
+    if n==0:
+        yield []
+        return
+
+    if n==1:
+        yield []
+        yield [0]
+        return
+
+    for subset in all_subsets(n-1):
+        yield subset
+        yield subset + [n-1] # sorted !!
+
+assert len(list(all_subsets(5))) == 2**5
+
+def factorial(n):
+    r = 1
+    for i in range(1, n+1):
+        r *= i
+    return r
+
+assert factorial(0) == 1
+assert factorial(1) == 1
+assert factorial(2) == 2
+assert factorial(3) == 2*3
+assert factorial(4) == 2*3*4
+
+
+def choose(items, n):
+    if n > len(items):
+        return
+    if n == 0:
+        yield ()
+        return
+    if n == 1:
+        for item in items:
+            yield (item,)
+        return
+    for i, item in enumerate(items):
+        for rest in choose(items[i+1:], n-1):
+            yield (item,)+rest
+
+assert len(list(choose(range(4), 1))) == 4
+assert len(list(choose(range(4), 2))) == 6
+assert len(list(choose(range(4), 3))) == 4
+
+
