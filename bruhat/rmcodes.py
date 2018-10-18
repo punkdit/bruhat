48f43f47b441604a34a1d00286178e98571b0b48
Simon Burton <simon@arrowtheory.com>
2018-10-18 11:23:14 +0100

    commit edits

--- bruhat/rmcodes.py
+++ bruhat/rmcodes.py
@@ -7,16 +7,74 @@ Build Reed-Muller codes.
 
 import numpy
 
-from solve import array2, zeros2, dot2, shortstr
+from solve import array2, zeros2, dot2, shortstr, rank, find_kernel, span
 from argv import argv
 from util import choose
 
 
-def main():
+class Code(object):
+    def __init__(self, G, H=None, d=None, check=True):
+        self.G = G.copy()
+        self.k, self.n = G.shape
+        self.d = d
 
-    r = argv.get("r", 1) # degree
-    m = argv.get("m", 3)
-    assert 0<r<=m
+        if H is None:
+            H = list(find_kernel(G))
+            H = array2(H)
+        if H.shape == (0,):
+            H.shape = (0, self.n)
+        self.H = H.copy()
+
+        if check:
+            self.check()
+
+    def check(self):
+        G, H = self.G, self.H
+        assert rank(G) == len(G)
+        A = dot2(H, G.transpose())
+        assert A.sum()==0
+
+    def __str__(self):
+        return "Code([[%s, %s, %s]])" % (self.n, self.k, self.d)
+
+    def dump(self):
+        G, H = self.G, self.H
+        print("G =")
+        print(shortstr(G))
+    
+        print("H =")
+        print(shortstr(H))
+
+    def is_selfdual(self):
+        G = self.G
+        x = dot2(G, G.transpose())
+        return x==0
+
+    def get_distance(self):
+        G = self.G
+        d = None
+        for v in span(G):
+            w = v.sum()
+            if w==0:
+                continue
+            if d is None or w<d:
+                d = w
+        return d
+
+    def puncture(self, i):
+        assert 0<=i<self.n
+        G = self.G
+        A = G[:i]
+        B = G[i+1:]
+        print(A.shape)
+        print(B.shape)
+        G = numpy.concatenate((A, B), axis=0)
+        return Code(G)
+    
+
+def build_rm(r, m):
+
+    assert 0<=r<=m
 
     n = 2**m # length
 
@@ -41,13 +99,49 @@ def main():
             basis.append(v)
         
     G = numpy.array(basis)
-    print(shortstr(G))
+    code = Code(G)
+    return code
+
+
+def test():
+
+    for m in range(2, 6):
+      for r in range(0, m+1):
+        code = build_rm(r, m)
+        assert code.n == 2**m
+        k = 1
+        for i in range(1, r+1):
+            k += len(list(choose(list(range(m)), i)))
+        assert code.k == k
+        if code.k < 12:
+            assert code.get_distance() == 2**(m-r)
+
+    print("OK")
+
+
+def main():
+    r = argv.get("r", 1) # degree
+    m = argv.get("m", 3)
+
+    code = build_rm(r, m)
+
+    print(code)
+    print("d =", code.get_distance())
+
+    code = code.puncture(3)
+
+    print(code)
+    print("d =", code.get_distance())
+
 
 
 
 if __name__ == "__main__":
 
-    main()
+    if argv.main:
+        main()
+    else:
+        test()
 
 
 
