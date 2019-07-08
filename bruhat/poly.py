42e7940002b09042e419e2a1d1fc48cbfb599d64
Simon Burton <simon@arrowtheory.com>
2019-07-08 20:49:04 +0100

    commit edits

--- bruhat/poly.py
+++ bruhat/poly.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from bruhat.util import cross
+from bruhat.theta import divisors
 
 
 def is_scalar(x):
@@ -36,8 +38,8 @@ class Poly(object):
             for v in dkey.values():
                 assert v
             coefs[key] = value
-        assert len(coefs) == len(cs)
-        self.cs = cs
+        #assert len(coefs) == len(cs), (coefs, cs)
+        self.cs = coefs
 
     @classmethod
     def promote(cls, item):
@@ -108,30 +110,133 @@ class Poly(object):
 
     def __str__(self):
         items = list(self.cs.items())
+        if not items:
+            return "0"
         items.sort()
         terms = []
         for (k, v) in items:
+            assert v != 0, self.cs
             ss = []
             for name, exp in k:
                 assert exp>0
-                if len(name)>1:
-                    name += " "
+                #if len(name)>1:
+                #    name += " "
                 if exp == 1:
                     ss.append(name)
                 else:
                     ss.append("%s^%s"%(name, exp))
-            s = ''.join(ss)
-            assert v != 0
-            if v==1:
-                terms.append(s)
+            s = ' '.join(ss)
+            if s:
+                if v==1:
+                    terms.append(s)
+                else:
+                    terms.append("%s*%s" % (v, s))
             else:
-                terms.append( "%s*%s" % (v, s))
-        s = "+ ".join(terms)
+                if v==1:
+                    terms.append("1")
+                else:
+                    terms.append("%s" % v)
+        s = " + ".join(terms)
         s = s.replace("+ -", "- ")
+        s = s.replace(" ^", "^")
+        if s and s[-1] == " ":
+            s = s[:-1]
         return s
 
     __repr__ = __str__
 
+
+def get_a(i):
+    if i==0:
+        return 0 # a_0 == 0
+    if i==1:
+        return 1 # a_1 == 1
+    return Poly("a_%d"%i)
+
+
+def get_b(i):
+    if i==0:
+        return 0 # b_0 == 0
+    if i==1:
+        return 1 # b_1 == 1
+    return Poly("b_%d"%i)
+
+
+def mul():
+    zero = Poly({})
+    #yield zero
+
+    n = 1
+    while 1:
+
+        #print("mul: n=%s"%n)
+        v = zero
+        for j in range(n+1):
+            k = n-j
+            v += get_a(j) * get_b(k)
+            #print(j, k, v)
+        yield v
+
+        n += 1
+
+
+def pow_b(n, i):
+    assert n>0
+    #print("pow_b(%d, %d)"%(n, i))
+    if n==1:
+        return get_b(i)
+    zero = Poly({})
+    v = zero
+    items = list(range(1, i+1))
+    for idxs in cross([items]*(n-1)):
+        total = sum(idxs)
+        if total >= i:
+            continue
+        #idxs.append(i - total)
+        last = i-total
+        #cs = dict((("b_%d"%idx, 1), 1) for idx in idxs)
+        p = get_b(last)
+        for idx in idxs:
+            p *= get_b(idx)
+        v += p
+    return v
+
+
+def dirichlet():
+    zero = Poly({})
+    n = 1
+    while 1:
+
+        v = zero
+        for j in divisors(n):
+            k = n//j
+            v += get_a(j) * get_b(k)
+        yield v
+
+        n += 1
+
+
+def compose():
+    #print("compose")
+    zero = Poly({})
+    #yield zero
+
+    i = 1
+    while 1:
+        n = 1
+        q = zero
+        while 1:
+            v = pow_b(n, i)
+            if v==0:
+                break
+            p = get_a(n)
+            #print(p*v, end= ", ")
+            q += p*v
+            n += 1
+        #print()
+        i += 1
+        yield q
+    #print()
 
 
 def test():
@@ -143,6 +248,8 @@ def test():
     xx = Poly({(("x", 2),) : 1})
     xy = Poly({(("x", 1), ("y", 1)) : 1})
 
+    assert str(one) == "1"
+    assert str(one+one) == "2"
     assert zero*x == zero
     assert x+zero == x
     assert one*x == x
@@ -157,8 +264,22 @@ def test():
         a += Poly("a_%d"%i)
         b += Poly("b_%d"%i)
 
-    print(a, b)
-    print(a*b)
+    for i in range(2, 6):
+        for n in range(1, 6):
+            vs = pow_b(n, i)
+            #print("pow_b(%d, %d) = %s" % (n, i, vs))
+            print(vs, end= ", ")
+        print()
+    print()
+
+
+    #gen = mul()
+    gen = compose()
+    #gen = dirichlet()
+    for i in range(1, 6):
+        p = gen.__next__()
+        print("compose(%d) = %s"%(i, p))
+        print()
 
 
 
