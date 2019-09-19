d237779d979334e65ee09c785ab68fbd6967a3a9
Simon Burton <simon@arrowtheory.com>
2019-09-19 15:30:59 +0100

    commit edits

--- bruhat/dev/qudit.py
+++ bruhat/dev/qudit.py
@@ -7,6 +7,7 @@ import numpy
 
 from bruhat import element
 from bruhat.action import mulclose
+from bruhat.util import cross
 from bruhat.element import CyclotomicRing, test
 from bruhat.vec import Space, Hom, Map
 from bruhat.argv import argv
@@ -40,7 +41,7 @@ def main():
     wI = numpy.zeros((d, d), dtype=object)
     X = numpy.zeros((d, d), dtype=object)
     Z = numpy.zeros((d, d), dtype=object)
-    Zs = numpy.zeros((d, d), dtype=object)
+    Zdag = numpy.zeros((d, d), dtype=object)
     S = numpy.zeros((d, d), dtype=object)
     Sdag = numpy.zeros((d, d), dtype=object)
     for j in range(d):
@@ -48,7 +49,7 @@ def main():
         wI[j, j] = w
         X[j, (j+1)%d] = 1
         Z[j, j] = w**j
-        Zs[j, j] = w**(d-j)
+        Zdag[j, j] = w**(d-j)
         S[j, j] = w**(j**2)
         Sdag[j, j] = w**(d - (j**2)%d)
 
@@ -57,9 +58,10 @@ def main():
 
     I = Map.from_array(I, hom)
     wI = Map.from_array(wI, hom)
+    Xdag = Map.from_array(X.transpose(), hom)
     X = Xs = Map.from_array(X, hom)
     Z = Map.from_array(Z, hom)
-    Zs = Map.from_array(Zs, hom)
+    Zdag = Map.from_array(Zdag, hom)
     S = Map.from_array(S, hom)
     Sdag = Map.from_array(Sdag, hom)
     Y = w*X*Z # ?
@@ -68,7 +70,12 @@ def main():
 
     pauli = mulclose([X, Z])
     pauli = set(pauli)
-    print(len(pauli))
+    print("pauli:", len(pauli))
+    assert Zdag in pauli
+    assert Xdag in pauli
+
+    assert Z*Zdag == I
+    assert X*Xdag == I
 
     if d>2:
         for j in range(1, d+1):
@@ -80,44 +87,63 @@ def main():
         assert X*X == I
         assert Z*Z == I
 
-    assert Z*X != X*Z
+    assert Z*X == (w**(d-1))*X*Z
 
     if d<6:
-        # slooooooow
+        # slow..
         lhs = atpow(X, d)
         rhs = atpow(Z, d)
         assert lhs * rhs == rhs * lhs
     
-    lhs = X@Xs
-    rhs = Z@Zs
+    lhs = X@X
+    rhs = Z@Zdag
     assert lhs * rhs == rhs * lhs
+
+    if 0:
+        n = 5
+        ops = [X, Xdag]
+        lhs = []
+        for op in cross([ops]*n):
+            op = reduce(matmul, op)
+            lhs.append(op) 
     
-    print(Y)
-    print(S*X*Sdag)
-    print(Y == S*X*Sdag)
+        ops = [Z, Zdag]
+        rhs = []
+        for op in cross([ops]*n):
+            op = reduce(matmul, op)
+            rhs.append(op) 
+    
+        for l in lhs:
+          for r in rhs:
+            if l*r == r*l:
+                print("/", end=" ", flush=True)
+            else:
+                print(".", end=" ", flush=True)
+        print()
+
+    #print(Z@Z@Z)
+    
+    #print(Y)
+    #print(S*X*Sdag)
+    #print(Y == S*X*Sdag)
     if d==3:
         assert (Y == S*X*Sdag)
     
-    op = S*X*Sdag
-    for g in pauli:
-        if op == g:
-            print("found")
-            break
-    else:
-        print("not found")
-    
-
     def is_cliff(A, Adag):
         for g in pauli:
             h = A*g*Adag
             if h not in pauli:
                 return False
         return True
+    assert is_cliff(S, Sdag)
 
-    print("is_cliff:", (S in pauli), is_cliff(S, Sdag))
+    #print("is_cliff:", (S in pauli), is_cliff(S, Sdag))
 
     #def is_third_level(A, Adag):
         
+
+    print("OK")
+
 
 
 if __name__ == "__main__":
