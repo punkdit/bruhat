0d53fc5696f9d67c9e624c0b68fd47f4d63c6004
Simon Burton <simon@arrowtheory.com>
2021-05-28 09:16:22 +0200

    commit edits

--- bruhat/rational.py
+++ bruhat/rational.py
@@ -64,41 +64,57 @@ assert list(tpl_range((1,2,1))) == [
     (1, 2, 0), (0, 0, 1), (1, 0, 1), (0, 1, 1), (1, 1, 1), (0, 2, 1), (1, 2, 1)]
 
 
-def divpoly(base, p, q, count=5):
-    # f(x) == p(x) / q(x)
-    # q(x)*f(x) == p(x)
-    p = Poly.promote(p, base)
-    q = Poly.promote(q, base)
-
-    if q.is_const():
-        q = q.get_const()
-        result = (1/q)*p
-        if result.is_const():
-            result = result.get_const() # demote
-        return result # <------------------- return
-
-    zero, one = base.zero, base.one
+class Rational(object):
+    def __init__(self, base, p, q, vs=None):
+        # f(x) == p(x) / q(x)
+        # q(x)*f(x) == p(x)
+        p = Poly.promote(p, base)
+        q = Poly.promote(q, base)
     
-    vs = p.get_vars() + q.get_vars()
-    vs = list(set(vs))
-    vs.sort()
-    n = len(vs)
-    assert n>0
+        zero, one = base.zero, base.one
+        
+        if vs is None:
+            vs = p.get_vars() + q.get_vars()
+            vs = list(set(vs))
+            vs.sort()
+        n = len(vs)
+        assert n>0
+    
+        #print("divpoly(%s, %s, %s)" % (p, q, vs))
+    
+        fs = {}
+        vzero = {v:zero for v in vs}
+    
+        top = p(**vzero)
+        bot = q(**vzero)
+        #print("top=%s, bot=%s" % (top, bot))
+        f0 = top/bot
+        #print("f0:", lstr(f0))
+        fs[(0,)*n] = f0
 
-    #print("divpoly(%s, %s, %s)" % (p, q, vs))
-
-    fs = {}
-    vzero = {v:zero for v in vs}
-
-    top = p(**vzero)
-    bot = q(**vzero)
-    #print("top=%s, bot=%s" % (top, bot))
-    f0 = top/bot
-    #print("f0:", lstr(f0))
-    fs[(0,)*n] = f0
-
-    for deg in range(count):
-        for idxs in tpl_range((count,)*(n-1)):
+        self.base = base
+        self.p = p
+        self.q = q
+        self.fs = fs
+        self.vzero = vzero
+        self.vs = vs
+        self.bot = bot
+    
+    def pump(self):
+        base = self.base
+        p = self.p
+        q = self.q
+        fs = self.fs
+        vzero = self.vzero
+        vs = self.vs
+        bot = self.bot
+        n = len(vs)
+    
+        deg = 0
+        for tpl in fs.keys():
+            deg = max(deg, sum(tpl))
+        deg += 1
+        for idxs in tpl_range((deg,)*(n-1)):
             total = sum(idxs) 
             if total > deg:
                 continue
@@ -117,9 +133,23 @@ def divpoly(base, p, q, count=5):
             assert fs.get(idxs) is None or fs.get(idxs) == f0
             fs[idxs] = f0
             #print(idxs, f0)
-
-    coeffs = {idx : fs[idx] // tpl_factorial(idx) for idx in fs.keys()}
-    return coeffs
+            #print("\t", idxs)
+    
+    def __getitem__(self, idx):
+        fs = self.fs
+        vs = self.vs
+        if type(idx) is int:
+            idx = idx,
+        assert len(idx) == len(vs)
+        for i in idx:
+            assert 0<=i
+        #print("__getitem__", idx)
+        while fs.get(idx) is None:
+            self.pump()
+        #coeffs = {idx : fs[idx] // tpl_factorial(idx) for idx in fs.keys()}
+        #return coeffs
+        val = fs[idx] // tpl_factorial(idx)
+        return val
 
 
 
@@ -136,16 +166,31 @@ def test():
     xx = Poly({(("x", 2),) : 1}, ring)
     xy = Poly({(("x", 1), ("y", 1)) : 1}, ring)
 
-    cs = divpoly(ring, one, (1-x)**2)
-    print(lstr(cs))
+    f = Rational(ring, one, (1-x)**2)
+    cs = [f[i] for i in range(5)]
+    assert cs == [1, 2, 3, 4, 5]
 
-    #assert cs == [1, 2, 3, 4, 5]
-    cs = divpoly(ring, 1-x**2, (1-x)**6)
-    print(lstr(cs))
-    #assert cs == [1, 6, 20, 50, 105]
+    f = Rational(ring, 1-x**2, (1-x)**6)
+    cs = [f[i] for i in range(5)]
+    assert cs == [1, 6, 20, 50, 105]
 
-    cs = divpoly(ring, 1-x*y, (1-x)**3 * (1-y)**3)
-    print(lstr(cs))
+    f = Rational(ring, 1-x*y, (1-x)**3 * (1-y)**3)
+    cs = [[f[i,j] for i in range(4)] for j in range(4)]
+    assert cs == [[1, 3, 6, 10], [3, 8, 15, 24], [6, 15, 27, 42], [10, 24, 42, 64]]
+
+
+    J = Poly("J", ring)
+    L = Poly("L", ring)
+    Li = Poly("Li", ring)
+    vs = [J, L, Li]
+    f = Rational(ring, one, (1-J*L)*(1-J*Li), "J L Li".split())
+    for i in range(4):
+      for j in range(4):
+        for k in range(4):
+            print(f[i, j, k], end=' ')
+        print()
+      print()
+
 
 
 if __name__ == "__main__":
