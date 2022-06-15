4a7bafa5719166f4e93e8963d7973f68b7df3b23
Simon Burton <simon@arrowtheory.com>
2022-06-15 03:31:33 +0200

    commit edits

---
+++ bruhat/thompson.py
@@ -0,0 +1,419 @@
+#!/usr/bin/env python3
+
+"""
+Implement Thompson's group F
+
+https://conf.math.illinois.edu/~mobeidin/thompson.pdf
+"""
+
+from functools import total_ordering
+
+@total_ordering
+class Dyadic(object):
+    """
+        Dyadic rational 
+        https://en.wikipedia.org/wiki/Dyadic_rational
+    """
+
+    def __init__(self, top, exp=0):
+        while top%2==0 and exp>0:
+            #print(top, exp)
+            top //= 2
+            exp -= 1
+        #print(top, exp)
+        self.top = int(top)
+        self.exp = int(exp)
+        # top / 2^exp
+
+    def __str__(self):
+        return "%s/%s"%(self.top, 2**self.exp)
+
+    @classmethod
+    def promote(cls, item):
+        if isinstance(item, Dyadic):
+            return item
+        return Dyadic(item)
+
+    def __eq__(self, other):
+        other = self.promote(other)
+        return (self.top, self.exp) == (other.top, other.exp)
+
+    def __lt__(self, other):
+        other = self.promote(other)
+        # self < other
+        a = other - self
+        return a.top > 0
+
+    def __add__(self, other):
+        other = self.promote(other)
+        a, b = self.top, self.exp
+        c, d = other.top, other.exp
+        top = 2**(d-min(b,d)) * a + 2**(b-min(b,d)) * c
+        exp = max(b, d)
+        return Dyadic(top, exp)
+
+    def __sub__(self, other):
+        other = self.promote(other)
+        a, b = self.top, self.exp
+        c, d = other.top, other.exp
+        top = 2**(d-min(b,d)) * a - 2**(b-min(b,d)) * c
+        exp = max(b, d)
+        return Dyadic(top, exp)
+
+    def __neg__(self):
+        return Dyadic(-self.top, self.exp)
+
+    def __mul__(self, other):
+        other = self.promote(other)
+        a, b = self.top, self.exp
+        c, d = other.top, other.exp
+        return Dyadic(a*c, b+d)
+
+    def __truediv__(self, other):
+        other = self.promote(other)
+        top = other.top
+        s = bin(top)[2:]
+        if s.count('1') != 1:
+            return NotImplemented
+        assert top > 0, "todo..."
+        exp = len(s) - 1
+        top = self.top * (2**other.exp)
+        exp = self.exp + exp
+        return Dyadic(top, exp)
+
+
+class Interval(object):
+    def __init__(self, a, b):
+        assert a<b, "not an interval: %s>=%s"%(a, b)
+        a = Dyadic.promote(a)
+        b = Dyadic.promote(b)
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return "[%s, %s]"%(self.a, self.b)
+
+    def __eq__(self, other):
+        return self.a==other.a and self.b==other.b
+
+
+#class Key(object):
+#    def __init__(self, idx=()):
+#        self.idx = idx
+#    def __eq__(self, other):
+#        if type(other) is tuple:
+#            other = Key(other)
+#        return self.idx == other.idx
+#    def __str__(self):
+#        return str(self.idx)
+#    __repr__ = __str__
+#    def __getitem__(self, i):
+#        return self.idx[i]
+#    def __len__(self):
+#        return len(self.idx)
+#    def __add__(self, other):
+#        if type(other) is tuple:
+#            other = Key(other)
+#        return Key(self.idx+other.idx)
+#    def __lt__(lhs, rhs):
+#        # depth first search
+#        if lhs.idx[:len(rhs.idx)] == rhs.idx: # idx startswith jdx
+#            return True
+#        return lhs.idx < rhs.idx
+
+#
+#def join(lhs, rhs):
+#    lkeys = lhs.keys().sort()
+#    rkeys = rhs.keys().sort()
+#    li = ri = 0
+#    while 1:
+#        if li < len(lkeys) and ri < len(rkeys):
+#            lk = lkeys[li]
+#            rk = rkeys[ri]
+#            lexpr = lhs[lk]
+#            rexpr = rhs[rk]
+#            # fark !!
+#        elif li < len(lkeys):
+#            lk = lkeys[li]
+#        elif ri < len(rkeys):
+#            rk = rkeys[ri]
+#        else:
+#            break
+#            
+
+from bruhat import unify
+
+class Tree(object):
+    tp = unify.Theory("T")
+    tp.add_op("*", 2)
+
+    def __mul__(self, other):
+        assert isinstance(other, Tree), other
+        return Pair(self, other)
+    def to_expr(self, prefix="v", tp=None):
+        tp = self.tp
+        ks = self.keys()
+        #ks.sort() # depth first search
+        send = {}
+        count = 0
+        for k in ks:
+            tree = self[k]
+            if isinstance(tree, Leaf):
+                send[k] = tp.get_var("%s_%s"%(prefix,count))
+                count += 1
+            elif isinstance(tree, Pair):
+                a, b = tree.a, tree.b
+                expr = send[k+(0,)] * send[k+(1,)]
+                send[k] = expr
+            else:
+                assert 0
+        return send[()]
+    @classmethod
+    def from_expr(cls, expr):
+        vs = expr.all_vars()
+        ns = dict((str(v), Leaf()) for v in vs)
+        tree = eval(str(expr), ns)
+        return tree
+    def find_carets(self):
+        items = {}
+        count = 0
+        for k,tree in self.items():
+            tree = self[k]
+            if type(tree) == Leaf:
+                count += 1
+            elif tree.is_caret():
+                assert count>1
+                items[count] = k
+        return items
+
+
+
+class Leaf(Tree):
+    def __init__(self, name="_"):
+        self.name = name
+    def __str__(self):
+        return self.name
+    def __eq__(self, other):
+        return isinstance(other, Leaf)
+    def copy(self):
+        return Leaf(self.name)
+    def __getitem__(self, key):
+        #assert type(key) is Key, key
+        if key==():
+            return self
+        raise IndexError
+    def keys(self):
+        #return [Key()]
+        return [()]
+    def items(self):
+        yield (), self
+    def sup(lhs, rhs):
+        if isinstance(rhs, Leaf):
+            return lhs.copy()
+        return rhs.copy()
+    def join(lhs, rhs, send={}, idx=0):
+        send[0] = rhs
+        idx += 1
+        return send, idx
+    def nleaves(self):
+        return 1
+    def is_caret(self):
+        return False
+    def send(self, key, tree, depth=0):
+        print("  "*depth+"send", self, key, tree)
+        assert key==(), "%s --> %s"%(key, tree)
+        return tree
+
+class Pair(Tree):
+    def __init__(self, a, b):
+        assert isinstance(a, Tree), a
+        assert isinstance(b, Tree), b
+        self.a = a.copy()
+        self.b = b.copy()
+    def __str__(self):
+        return "(%s*%s)"%(self.a, self.b)
+    def __eq__(self, other):
+        return isinstance(other,Pair) and self.a==other.a and self.b==other.b
+    def copy(self):
+        return Pair(self.a, self.b)
+    def __getitem__(self, key):
+        #assert type(key) is Key, key
+        if key==():
+            return self
+        head, tail = key[0], key[1:]
+        return [self.a, self.b][head][tail]
+    def keys(self):
+        #ks = [Key()]
+        ks = []
+        for key in self.a.keys():
+            #ks.append(Key((0,))+key)
+            ks.append((0,)+key)
+        for key in self.b.keys():
+            #ks.append(Key((1,))+key)
+            ks.append((1,)+key)
+        ks.append(())
+        return ks
+    def items(self):
+        for (k,v) in self.a.items():
+            #assert self.a[k] == v
+            yield (0,)+k, v
+        for (k,v) in self.b.items():
+            #assert self.b[k] == v
+            yield (1,)+k, v
+        yield (), self
+    def sup(lhs, rhs):
+        if isinstance(rhs, Leaf):
+            return lhs.copy()
+        a = lhs.a.sup(rhs.a)
+        b = lhs.b.sup(rhs.b)
+        return Pair(a, b)
+    def join(lhs, rhs, send={}, idx=0):
+        #la, lb = lhs.a, lhs.b
+        #ra, rb = rhs.a, rhs.b
+        #???
+        return send, idx
+    def nleaves(self):
+        return self.a.nleaves() + self.b.nleaves()
+    def is_caret(self):
+        return self.a.__class__ is Leaf and self.b.__class__ is Leaf
+    def send(self, key, tree, depth=0):
+        #print("  "*depth+"send", self, key, tree)
+        assert self[key]
+        if key==():
+            return tree
+        head, tail = key[0], key[1:]
+        children = [self.a, self.b]
+        children[head] = children[head].send(tail, tree, depth+1)
+        return Pair(*children)
+
+
+def cancel_carets(lhs, rhs):
+    while 1:
+        #print("cancel_carets", lhs, rhs)
+        l = lhs.find_carets()
+        r = rhs.find_carets()
+        #print("\t", l, r)
+        items = set(l.keys()) & set(r.keys())
+        if not items:
+            break
+        i = min(items) # deterministic
+        lhs = lhs.send( l[i], Leaf() ) # remove caret
+        rhs = rhs.send( r[i], Leaf() ) # remove caret
+    return lhs, rhs
+    
+
+class Assoc(object):
+    "Element of Thompson group F"
+    def __init__(self, tgt, src):
+        assert isinstance(tgt, Tree)
+        assert isinstance(src, Tree)
+        assert tgt.nleaves() == src.nleaves()
+        tgt, src = cancel_carets(tgt, src)
+        self.tgt = tgt
+        self.src = src
+    def __str__(self):
+        return "%s<--%s"%(self.tgt, self.src)
+    def __eq__(self, other):
+        return self.tgt == other.tgt and self.src == other.src
+    def __mul__(l, r):
+        # l.tgt <--- l.src == r.tgt <--- r.src
+        ltgt = l.tgt.to_expr("l")
+        lsrc = l.src.to_expr("l")
+        rtgt = r.tgt.to_expr("r")
+        rsrc = r.src.to_expr("r")
+        #print("Assoc.__mul__", ltgt, "<---", lsrc, "===", rtgt, "<---", rsrc)
+    
+        send = unify.Expr.unify(lsrc, rtgt)
+        #print(send)
+        tgt, src = ltgt.subs(send), rsrc.subs(send)
+        #print("Assoc.__mul__", tgt, "<---", src)
+        tgt = Tree.from_expr(tgt)
+        src = Tree.from_expr(src)
+        #print("Assoc.__mul__", tgt, "<---", src)
+        return Assoc(tgt, src)
+    def __invert__(self):
+        return Assoc(self.src, self.tgt)
+
+
+def bracket(g, h):
+    return (~g)*(~h)*g*h
+
+
+def test():
+
+    a = Dyadic(2)
+    b = a/4
+    assert b == Dyadic(2, 2)
+    assert b == Dyadic(1, 1)
+    assert str(b) == "1/2"
+    assert Dyadic(3*256, 4) == 3*256//(2**4)
+
+    a = Dyadic(7)
+    b = Dyadic(1)/2
+    assert a+b == Dyadic(15)/2
+    assert a-b == Dyadic(13)/2
+    assert a*b == Dyadic(7)/2
+
+    assert b < a
+    assert b <= a
+    assert -b > -a
+
+    assert str(Interval(b, a)) == "[1/2, 7/1]"
+
+def test_tree():
+    x = Leaf()
+    lhs = x*(x*x)
+    rhs = (x*x)*x
+    #print(lhs, rhs)
+    #for key in lhs.keys():
+    #    print(key, lhs[key])
+    #print(lhs.sup(rhs))
+    lhs, rhs = x*((x*x)*x), (x*x)*x
+    #print(lhs.sup(rhs))
+
+    lhs, rhs = x*((x*x)*x), (x*x)*x
+    ks = lhs.keys()
+    #ks.sort()
+    assert ks == [(0,), (1, 0, 0), (1, 0, 1), (1, 0), (1, 1), (1,), ()]
+
+    assert lhs.find_carets() == {3: (1,0)}
+
+    
+
+    s = ((x*x)*x)*x
+    t = (x*x)*(x*x)
+
+    assert t.find_carets() == {2: (0,), 4: (1,)}
+    assert s.find_carets() == {2: (0, 0)}
+    t, s = cancel_carets(t, s)
+    assert t == x*(x*x), t
+    assert s == (x*x)*x, s
+
+    I = Assoc( x, x )
+    assert I == I
+    assert I*I == I
+    F = Assoc( (x*x)*x, (x*x)*x )
+    assert F == I
+
+    A = Assoc( (x*x)*x, x*(x*x) )
+    B = Assoc( x*((x*x)*x), x*(x*(x*x)) )
+    #print(A, B)
+
+    assert A*A == Assoc( (((x*x)*x)*x), (x*(x*(x*x))) )
+
+    assert I*A == A
+    assert A*I == A
+    assert A*~A == I
+    assert ~A*A == I
+
+    lhs = bracket( A*~B, ~A*B*A )
+    rhs = bracket( A*~B, (~A)*(~A)*B*A*A )
+    assert lhs == rhs
+
+
+if __name__ == "__main__":
+    test()
+    test_tree()
+    print("OK\n")
+
+
