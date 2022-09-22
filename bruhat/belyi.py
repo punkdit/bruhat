72cd7ad0f141ae0ea61921eb5304b636bbc59df0
Simon Burton <simon@arrowtheory.com>
2022-09-22 09:53:03 +0100

    commit edits

--- bruhat/belyi.py
+++ bruhat/belyi.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 
+"""
+Build system of equations for finding Belyi projection
+from vertex degrees of a dessin.
+"""
+
 from sage.all_cmdline import RationalField, PolynomialRing
 from sage.all_cmdline import *
 
 
-x = PolynomialRing(RationalField(), 'x').gen()
-f = (x**3 - 1)**2-(x**2-1)**2
-print( f.factor() )
+def test():
+    x = PolynomialRing(RationalField(), 'x').gen()
+    f = (x**3 - 1)**2-(x**2-1)**2
+    print( f.factor() )
+
+
+def main():
+
+    pass
+
+
+
+if __name__ == "__main__":
+
+    main()
 
 
 
