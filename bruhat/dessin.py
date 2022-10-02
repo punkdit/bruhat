3c6cdb94a58e7b1ef46daffb7a598e02cb83e563
Simon Burton <simon@arrowtheory.com>
2022-10-02 14:24:20 +0100

    commit edits

--- bruhat/dessin.py
+++ bruhat/dessin.py
@@ -13,11 +13,13 @@ from operator import matmul
 import numpy
 from numpy import alltrue, zeros, dot
 
+from bruhat.util import cross
 from bruhat.solve import array2, shortstr, dot2, linear_independent
 from bruhat.action import Perm, Group, Coset, mulclose, close_hom, is_hom
 from bruhat.todd_coxeter import Schreier
 from bruhat.argv import argv
 from bruhat.solve import shortstr, zeros2, dot2, array2, solve
+from bruhat.smap import SMap
 
 from huygens.namespace import *
 from huygens.pov import Mat
@@ -628,6 +630,7 @@ def make_dot(graph, cmd="neato"):
     #open("schreier.pdf", "w").write(data)
 
 
+
 def build_group(idx=0, halve=False):
     # start with hyperbolic Coxeter reflection group: a--5--b--5--c
     # Then we add another generator "d" that halves these triangles.
@@ -694,6 +697,7 @@ def parse(s, a, b, c):
     s = s.replace(" ", "")
     return eval(s)
 
+
 def build_group_524():
     ngens = 3
     a, b, c, = range(ngens)
@@ -701,7 +705,6 @@ def build_group_524():
         (a,)*2, (b,)*2, (c,)*2,
         (a,b)*5, (b,c)*4, (a,c)*2,
     ]
-
     a, b, c = (a,), (b,), (c,)
 
     relss = []
@@ -735,7 +738,243 @@ def build_group_524():
     graph = Schreier(ngens, rels_524 + relss[idx])
     graph.build(maxsize=100000)
     n = len(graph)
-    print(n)
+    print("|G| =", n)
+    G = graph.get_group()
+    return G
+
+
+def build_group_624():
+    ngens = 3
+    a, b, c, = range(ngens)
+    rels_624 = [
+        (a,)*2, (b,)*2, (c,)*2,
+        (a,b)*6, (b,c)*4, (a,c)*2,
+    ]
+    a, b, c = (a,), (b,), (c,)
+
+    relss = []
+
+    rels = parse("""
+        [b*c*b*a*b*c*b^-1*a^-1*b^-1*c^-1*b^-1*a^-1, 
+        c*b*a*b*c*b*a^-1*b^-1*c^-1*b^-1*a^-1*b^-1,
+        a*c*b*a*b*c*b*a^-1*b^-1*c^-1*(b^-1*a^-1)^2, 
+        b*a*c*b*a*b*c*b*a^-1*b^-1*c^-1*(b^-1*a^-1)^2*b^-1,
+        a*b*a*c*b*a*b*c*b*a^-1*b^-1*c^-1*(b^-1*a^-1)^3,
+        (b*a)^2*c*b*a*b*c*b^-1*a^-1*b^-1*c^-1*(a^-1*b^-1)^2*a^-1,
+        (c*b*a)^2*b*c*b*a^-1*b^-1*c^-1*(b^-1*a^-1)^2*b^-1*c^-1,
+        (a*c*b)^2*a*b*c*b*a^-1*b^-1*c^-1*(b^-1*a^-1)^2*b^-1*c^-1*a^-1,
+        b*(c*b*a)^2*b*c*b*a^-1*b^-1*c^-1*(b^-1*a^-1)^2*b^-1*c^-1*b^-1,
+        (b*a*c)^2*b*a*b*c*b*a^-1*b^-1*c^-1*(b^-1*a^-1)^2*b^-1*c^-1*a^-1*b^-1,
+        a*(b*a*c)^2*b*a*b*c*b*a^-1*b^-1*c^-1*(b^-1*a^-1)^2*b^-1*c^-1*a^-1*b^-1*a^-1,
+        c*b*a*(b*a*c)^2*b*a*b*c^-1*b^-1*(a^-1*b^-1*c^-1)^2*(a^-1*b^-1)^2 ]
+    """, a, b, c)
+    relss.append(rels)
+
+    idx = argv.get("idx", 0)
+    graph = Schreier(ngens, rels_624 + relss[idx])
+    graph.build(maxsize=100000)
+    n = len(graph)
+    print("|G| =", n)
+    G = graph.get_group()
+    return G
+
+
+class QCode(object):
+    def __init__(self, H):
+        m, n, k = H.shape
+        assert k == 2
+        assert H.max() <= 1
+        self.H = H
+        self.m = m
+        self.n = n
+        self.shape = m, n
+        self.check()
+
+    def check(self):
+        H = self.H
+        m, n = self.shape
+        J = H.copy()
+        J[:, :, :] = J[:, :, [1,0]]
+        H = H.view()
+        H.shape = J.shape = (m, 2*n)
+        assert dot2(H, J.transpose()).sum() == 0, "not symplectic"
+
+    def apply(self, idx, gate):
+        H = self.H.copy()
+        H[:, idx] = dot(self.H[:, idx], gate) % 2
+        return QCode(H)
+
+    def apply_H(self, idx):
+        # swap X<-->Z on bit idx
+        H = array2([[0,1],[1,0]])
+        return self.apply(idx, H)
+
+    def apply_S(self, idx):
+        # swap X<-->Y
+        S = array2([[1,1],[0,1]])
+        return self.apply(idx, S)
+
+    def apply_SH(self, idx):
+        # X-->Z-->Y-->X 
+        SH = array2([[0,1],[1,1]])
+        return self.apply(idx, SH)
+
+    def __str__(self):
+        smap = SMap()
+        m, n = self.shape
+        H = self.H
+        for i in range(m):
+          for j in range(n):
+            x, z = H[i, j]
+            c = '.'
+            if x and z:
+                c = 'Y'
+            elif x:
+                c = 'X'
+            elif z:
+                c = 'Z'
+            smap[i,j] = c
+        return str(smap)
+
+    def shortstr(self):
+        H = self.H.view()
+        H.shape = (self.m, 2*self.n)
+        return shortstr(H)
+
+    
+def find_code(H):
+    "build a QCode from face--vert adjacency matrix"
+    import z3
+    from z3 import Bool, And, Or, Not, Implies, If, Solver
+
+    m, n = H.shape
+    vs = {}
+    xvars = {}
+    zvars = {}
+    yvars = {}
+
+    clauses = []
+    solver = Solver()
+
+    add = solver.add
+
+    for i in range(m):
+      for j in range(n):
+        if H[i, j] == 0:
+            continue
+        X = Bool("X_%d_%d"%(i,j))
+        Z = Bool("Z_%d_%d"%(i,j))
+        vs[i, j] = (X, Z)
+        xvars[i,j] = X
+        zvars[i,j] = Z
+        yvars[i,j] = And(X, Z)
+        # X or Z (or both)
+        add(Or(X, Z))
+
+    # each qubit has X & Z check
+    for j in range(n): # cols
+        idxs = [i for i in range(m) if H[i, j]] # rows
+        assert len(idxs) > 1, idxs
+        add(Or(*[xvars[i,j] for i in idxs]))
+        add(Or(*[zvars[i,j] for i in idxs]))
+        add(Not(And(*[yvars[i,j] for i in idxs])))
+        # yes?
+
+#    # commuting stabilizers
+#    for i0 in range(m):
+#      for i1 in range(i0+1, m):
+#        bits = [j for j in range(n) if H[i0,j]==H[i1,j]>0]
+#        if not bits:
+#            continue
+#        print(bits)
+#        for lhs in cross([(0,1)]*len(bits)):
+#          lvars = [vs[i0, k][l] for (k,l) in zip(bits, lhs)]
+#          #print(lhs, lvars)
+#          for rhs in cross([(0,1)]*len(bits)):
+#            rvars = [vs[i1, k][r] for (k,r) in zip(bits, rhs)]
+#            syndrome = len([idx for idx in range(len(bits)) if lhs[idx]!=rhs[idx]]) % 2
+#            #print('\t', rhs, rvars, syndrome)
+#            if syndrome:
+#                clause = Not(And(*lvars, *rvars))
+#                #print('\t', clause)
+#                solver.add(clause)
+
+    # commuting stabilizers
+    for i0 in range(m):
+      for i1 in range(i0+1, m):
+        bits = [j for j in range(n) if H[i0,j]==H[i1,j]>0]
+        if not bits:
+            continue
+        #print(bits)
+        for bits0 in cross([((1,0),(0,1),(1,1))]*len(bits)):
+         for bits1 in cross([((1,0),(0,1),(1,1))]*len(bits)):
+            syndrome = sum(l[0]*r[1]+l[1]*r[0] for (l,r) in zip(bits0, bits1))
+            #print(bits0, bits1, syndrome)
+            if syndrome%2 == 0:
+                continue # allowed
+            clause = []
+            for j, jj in enumerate(bits):
+                X0, Z0 = vs[i0, jj]
+                X1, Z1 = vs[i1, jj]
+                clause.append(X0 if bits0[j][0] else Not(X0))
+                clause.append(Z0 if bits0[j][1] else Not(Z0))
+                clause.append(X1 if bits1[j][0] else Not(X1))
+                clause.append(Z1 if bits1[j][1] else Not(Z1))
+            clause = Not(And(*clause)) # not allowed
+            solver.add(clause)
+
+    result = solver.check()
+    if result != z3.sat:
+        print(result)
+        return None
+
+    H = zeros2(m, n, 2)
+    model = solver.model()
+    for key, XZ in vs.items():
+        X, Z = XZ
+        X = model.evaluate(X)
+        Z = model.evaluate(Z)
+        assert X or Z
+        i, j = key
+        if X:
+            H[i, j, 0] = 1
+        if Z:
+            H[i, j, 1] = 1
+    code = QCode(H)
+    return code
+
+    H = numpy.empty((m, n), dtype=object)
+    H[:] = '.'
+    model = solver.model()
+    for key, XZ in vs.items():
+        #for v in XZ:
+            #print(v, model.evaluate(v))
+        X, Z = XZ
+        X = model.evaluate(X)
+        Z = model.evaluate(Z)
+        assert X or Z
+        if X and Z:
+            H[key] = 'Y'
+        elif X:
+            H[key] = 'X'
+        elif Z:
+            H[key] = 'Z'
+
+    print("H =", H.shape)
+    H = ("\n".join(''.join(row) for row in H))
+    print(H)
+    return H
+
+
+def test_surface():
+
+    sides = argv.get("sides", 5)
+    if sides==5:
+        G = build_group_524()
+    elif sides==6:
+        G = build_group_624()
+    else:
+        assert 0
 
     if argv.dot:
         graph = Schreier(ngens, rels_524 + relss[idx])
@@ -744,7 +983,6 @@ def build_group_524():
         make_dot(graph, "neato")
         return
 
-    G = graph.get_group()
     a, b, c = G.gens
     red, blue, green = G.gens
 
@@ -768,10 +1006,16 @@ def build_group_524():
             A[i, j] = len(lr)>0
         return A
 
-    #g = blue*green*blue*green # divide a vert in half
-    g = red*green # divide an edge in half
-    assert g.order() == 2
-    H = Group.generate([g, g*g])
+    if argv.fold_vert:
+        g = blue*green*blue*green # divide a vert in half
+        assert g.order() == 2
+    elif argv.fold_edge:
+        g = red*green # divide an edge in half
+        assert g.order() == 2
+    else:
+        g = G.identity
+
+    H = Group.generate([g])
     #pairs = G.left_cosets(H)
     act = G.action_subgroup(H)
     faces = act.orbits(Group.generate([blue, red]))
@@ -814,6 +1058,14 @@ def build_group_524():
     print(AB.shape)
     print(AB)
 
+    if argv.find_code:
+        code = find_code(AB)
+        print(code)
+        code = code.apply_S(0)
+        print()
+        print(code)
+        print(code.shortstr())
+        return
 
     labels = string.ascii_lowercase
 
@@ -912,14 +1164,11 @@ def build_group_524():
     print(shortstr(Hz))
     assert dot2(Hx, Hz.transpose()).sum() == 0
 
-    
+
     
 
 
 def make_hyperbolic():
-
-    G = build_group_524()
-    return
 
     G = build_group(2)
     H = build_group(1)
@@ -960,13 +1209,32 @@ if __name__ == "__main__":
 
     start_time = time()
 
-    #test()
-    #test_real_pauli()
-    #render_1()
-    #render_2()
-    #main()
-    #test_dessins()
-    make_hyperbolic()
+
+    profile = argv.profile
+    name = argv.next()
+    _seed = argv.get("seed")
+    if _seed is not None:
+        print("seed(%s)"%(_seed))
+        seed(_seed)
+
+    if profile:
+        import cProfile as profile
+        profile.run("%s()"%name)
+
+    elif name is not None:
+        fn = eval(name)
+        fn()
+
+    else:
+        test()
+        #test_real_pauli()
+        #render_1()
+        #render_2()
+        #main()
+        #test_dessins()
+        #make_hyperbolic()
+
+
 
     t = time() - start_time
     print("finished in %.3f seconds"%t)
