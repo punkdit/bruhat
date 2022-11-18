d908a9bd85dda7ae8664311b6e026076cf3b1140
Simon Burton <simon@arrowtheory.com>
2022-11-18 11:37:05 +0000

    commit edits

--- bruhat/combinatorial.py
+++ bruhat/combinatorial.py
@@ -10,14 +10,13 @@ Weyl groups...
 import sys, os
 from time import time
 start_time = time()
-import random
-from random import randint, choice
 from functools import reduce
 from operator import add, mul
 from string import ascii_lowercase
+letters = ascii_lowercase
 
-from bruhat.action import Perm, Group, Coset, mulclose
-from bruhat.action import Group
+from bruhat.action import Perm, Group, Coset, mulclose, Action
+from bruhat.smap import SMap
 from bruhat.argv import argv
 
 
@@ -62,9 +61,12 @@ def build_B(n):
 
 def test_weyl():
 
-    Hs = {}
-    Gs = {}
-    for n in [0, 1, 2, 3, 4]:
+    N = 6
+
+    Gs = {} # Group's
+    Hs = {} # stabilizer Coset's
+    Xs = {} # Action's
+    for n in range(N):
         G = build_A(n)
         Gs[n] = G
         items = G.items
@@ -77,10 +79,128 @@ def test_weyl():
                 if fix==send:
                     H.append(g)
             Hs[n, m] = H
-            print(len(G) // len(H), end=" ")
-        print()
+            H = Coset(H, G.items)
+            X = G.action_subgroup(H)
+            X.basepoint = H
+            Xs[n, m] = X
 
+    def get_pos(n, m):
+        K = 4
+        return n, m*K + int(round((3-n/2)*K))
+
+    smap = SMap()
+    for n in range(N):
+        for m in range(n+1):
+            G = Gs[n]
+            H = Hs[n, m]
+            smap[get_pos(n, m)] = str(len(G)//len(H))
+    print(smap)
     print()
+
+    def check_group_hom(G0, G1, hom):
+        assert len(G0) == len(hom)
+        for g in G0:
+          for h in G0:
+            assert hom[g*h] == hom[g]*hom[h]
+        assert len(set(hom.values())) == len(hom), "not injective"
+
+    smap = SMap()
+    lhoms = {}
+    rhoms = {}
+    for n in range(N-1):
+        # construct two Group hom's, G0 --> G1
+        G0 = Gs[n]
+        G1 = Gs[n+1]
+        items = G1.items
+
+        # add new item on the right
+        hom = {}
+        for g in G0:
+            perm = dict(g.perm)
+            perm[items[-1]] = items[-1]
+            h = Perm(perm, items)
+            assert h in G1
+            hom[g] = h
+        #print("hom:", len(hom))
+        check_group_hom(G0, G1, hom)
+        rhoms[n] = hom
+
+        # add new item on the left
+        lookup = {src : letters[letters.index(src)+1] for src in G0.items}
+        hom = {}
+        for g in G0:
+            perm = {lookup[src] : lookup[g.perm[src]] for src in G0.items}
+            #print(g.perm, perm)
+            perm[items[0]] = items[0]
+            #print(g.perm, perm)
+            h = Perm(perm, items)
+            assert h in G1
+            hom[g] = h
+        check_group_hom(G0, G1, hom)
+        #print("hom:", len(hom))
+        lhoms[n] = hom
+
+    lfuncs = {}
+    rfuncs = {}
+    for n in range(N-1):
+      for m in range(n+1):
+        print("\n%d choose %d" % (n, m))
+
+        # construct a right map
+        src = Xs[n, m]
+        tgt = Xs[n+1, m+1]
+        print(len(src.items), ">-->", len(tgt.items))
+        func = {} # map src --> tgt
+        for x in src.items:
+            # find a coset representative
+            for g in src.G:
+                if src[g](src.basepoint) == x:
+                    break
+            else:
+                assert 0
+            h = lhoms[n][g]
+            y = tgt[h](tgt.basepoint)
+            func[x] = y
+        assert len(func) == len(src.items)
+        assert len(set(func.values())) == len(func)
+        rfuncs[n, m] = func
+
+        # construct a left map
+        src = Xs[n, m]
+        tgt = Xs[n+1, m]
+        func = {} # map src --> tgt
+        for x in src.items:
+            # find a coset representative
+            for g in src.G:
+                if src[g](src.basepoint) == x:
+                    break
+            else:
+                assert 0
+            h = lhoms[n][g]
+            y = tgt[h](tgt.basepoint)
+            func[x] = y
+        assert len(func) == len(src.items)
+        assert len(set(func.values())) == len(func)
+        lfuncs[n, m] = func
+
+    n, m = 4, 2
+    rmap = rfuncs[n-1, m-1]
+    lmap = lfuncs[n-1, m]
+
+    tgt = Xs[n, m]
+    print(len(tgt.items))
+    found = set(tgt.items)
+    image = set(rmap.values())
+    print(len(set(rmap.keys())), "-->", len(image))
+    for item in rmap.values():
+        print(item in found)
+        #assert item in found
+        #found.remove(item)
+    for item in lmap.values():
+        assert item in found
+        found.remove(item)
+
+    return
 
     def show(coset):
         items = []
@@ -93,11 +213,9 @@ def test_weyl():
         return "[%s]"%(", ".join(items))
 
     def dump(n, m):
-        G = Gs[n]
-        H = Hs[n, m]
-        X = G.left_cosets(H)
-        print("|%d choose %d| = %d"%(n, m, len(X)))
-        for x in X:
+        X = Xs[n, m]
+        print("|%d choose %d| = %d"%(n, m, len(X.items)))
+        for x in X.items:
             print('\t', show(x))
     dump(3, 2)
     dump(4, 2)
