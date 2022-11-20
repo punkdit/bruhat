d639a2e09ee6cf7ef20ad2ea6d844d345e0ee0c5
Simon Burton <simon@arrowtheory.com>
2022-11-20 11:56:34 +0000

    commit edits

--- bruhat/combinatorial.py
+++ bruhat/combinatorial.py
@@ -10,7 +10,7 @@ Weyl groups...
 import sys, os
 from time import time
 start_time = time()
-from functools import reduce
+from functools import reduce, lru_cache
 from operator import add, mul
 from string import ascii_lowercase
 letters = ascii_lowercase
@@ -19,44 +19,7 @@ from bruhat.action import Perm, Group, Coset, mulclose, Action
 from bruhat.smap import SMap
 from bruhat.argv import argv
 
-
-def build_A(n):
-    letters = ascii_lowercase[:n]
-    items = list(letters)
-
-    gen = []
-    for i in range(n-1):
-        perm = {c:c for c in items}
-        perm[items[i]] = items[i+1]
-        perm[items[i+1]] = items[i]
-        gen.append(Perm(perm, items))
-    G = Group.generate(gen, items=items)
-    return G
-
-
-def build_B(n):
-    letters = ascii_lowercase[:n]
-    items = ["+"+c for c in letters] + ["-"+c for c in letters]
-    pairs = [("+"+c, "-"+c) for c in letters]
-
-    gen = []
-    for i in range(n-1):
-        perm = {c:c for c in items}
-        a, b = pairs[i]
-        c, d = pairs[i+1]
-        perm[a] = c
-        perm[c] = a
-        perm[b] = d
-        perm[d] = b
-        gen.append(Perm(perm, items))
-    if n:
-        perm = {c:c for c in items}
-        a, b = pairs[-1]
-        perm[a] = b
-        perm[b] = a
-        gen.append(Perm(perm, items))
-    G = Group.generate(gen, items=items)
-    return G
+cache = lru_cache(maxsize=None)
 
 def show(coset):
     items = []
@@ -68,65 +31,97 @@ def show(coset):
         items.append("{%s}"%s)
     return "[%s]"%(", ".join(items))
 
+def check_group_hom(G0, G1, hom):
+    assert len(G0) == len(hom)
+    for g in G0:
+      for h in G0:
+        assert hom[g*h] == hom[g]*hom[h]
+    assert len(set(hom.values())) == len(hom), "not injective"
 
-def test_weyl():
 
-    N = 6
+def check_injection(src, tgt, func):
+    assert set(func.keys()) == set(src)
+    output = set(func.values())
+    assert len(output) == len(func)
+    assert output.issubset(tgt)
 
-    Gs = {} # Group's
-    Hs = {} # stabilizer Coset's
-    Js = {} # stabilizer Coset's
-    Xs = {} # Action's
-    Ys = {} # Action's
-    ks = {} # send Y --> X
-    for n in range(N):
-        G = build_A(n)
-        Gs[n] = G
+
+
+class Pascal(object):
+    "Pascal's triangle for thin geometry Weyl group"
+
+    def __init__(self):
+        pass
+
+    def str(self, N):
+        def get_pos(n, m):
+            K = 4
+            return n, m*K + int(round((3-n/2)*K))
+    
+        smap = SMap()
+        for n in range(N):
+            for m in range(n+1):
+                G = Gs[n]
+                H = Hs[n, m]
+                smap[get_pos(n, m)] = str(len(G)//len(H))
+        return str(smap)
+
+    def get_G(self, n):
+        assert 0, "abstract base class"
+
+    @cache
+    def get_X(self, n, m):
+        # fix 0,....,m-1
+        G = self.get_G(n)
         items = G.items
-        #print(len(G))
-        for m in range(n+1):
-            # fix 0,....,m-1
-            H = []
-            fix = {items[j] for j in range(m)}
-            for g in G:
-                send = {g[x] for x in fix}
-                if fix==send:
-                    H.append(g)
-            Hs[n, m] = H
-            H = Coset(H, G.items)
-            X = G.action_subgroup(H)
-            assert X.basepoint == H
-            Xs[n, m] = X
-        for m in range(n):
-            # fix 1,...,m
-            J = []
-            fix = {items[j] for j in range(1,m+1)}
-            for g in G:
-                send = {g[x] for x in fix}
-                if fix==send:
-                    J.append(g)
-            Js[n, m] = J
-            J = Coset(J, G.items)
-            Y = G.action_subgroup(J)
-            assert Y.basepoint == J
-            Ys[n, m] = Y
+        assert 0<=m<=n
+        H = []
+        fix = {items[j] for j in range(m)}
+        for g in G:
+            send = {g[x] for x in fix}
+            if fix==send:
+                H.append(g)
+        #Hs[n, m] = H
+        H = Coset(H, G.items)
+        X = G.action_subgroup(H)
+        assert X.basepoint == H
+        #Xs[n, m] = X
+        return X
 
-            assert len(items) == n
-            k = {items[j]:items[j-1] for j in range(1,n)}
-            k[items[0]] = items[-1]
-            k = Perm(k, items)
-            assert k in G
-            ks[n,m] = k
+    @cache
+    def get_Y(self, n, m):
+        # fix 1,...,m
+        G = self.get_G(n)
+        items = G.items
+        assert 0<=m<n
+        J = []
+        fix = {items[j] for j in range(1,m+1)}
+        for g in G:
+            send = {g[x] for x in fix}
+            if fix==send:
+                J.append(g)
+        #Js[n, m] = J
+        J = Coset(J, G.items)
+        Y = G.action_subgroup(J)
+        assert Y.basepoint == J
+        #Ys[n, m] = Y
+        return Y
 
-    isos = {} # send Y --> X
-    for n in range(N):
-     for m in range(n):
-        src = Ys[n, m]
-        tgt = Xs[n, m]
+    @cache
+    def get_iso(self, n, m):
+        src = self.get_Y(n, m)
+        tgt = self.get_X(n, m)
         #assert src.isomorphic(tgt) # yes
 
-        # Construct an isomorphism of G-sets
-        k = ks[n, m]
+        G = self.get_G(n)
+        items = G.items
+        assert len(items) == n
+        k = {items[j]:items[j-1] for j in range(1,n)}
+        k[items[0]] = items[-1]
+        k = Perm(k, items)
+        assert k in G
+
+        # Construct an isomorphism of G-sets X-->Y
         send = {}
         for y in src.items:
             g = src.repr[y]
@@ -134,40 +129,20 @@ def test_weyl():
             assert x in tgt.items
             send[y] = x
         src.check_isomorphism(tgt, send)
-        isos[n, m] = send
+        #isos[n, m] = send
 
         # this also works
         #iso = iter(src.isomorphisms(tgt)).__next__()
         #assert iso is not None
         #isos[n, m] = iso
 
-    def get_pos(n, m):
-        K = 4
-        return n, m*K + int(round((3-n/2)*K))
+        return send
 
-    smap = SMap()
-    for n in range(N):
-        for m in range(n+1):
-            G = Gs[n]
-            H = Hs[n, m]
-            smap[get_pos(n, m)] = str(len(G)//len(H))
-    print(smap)
-    print()
-
-    def check_group_hom(G0, G1, hom):
-        assert len(G0) == len(hom)
-        for g in G0:
-          for h in G0:
-            assert hom[g*h] == hom[g]*hom[h]
-        assert len(set(hom.values())) == len(hom), "not injective"
-
-    smap = SMap()
-    lhoms = {}
-    rhoms = {}
-    for n in range(N-1):
-        # construct two Group hom's, G0 --> G1
-        G0 = Gs[n]
-        G1 = Gs[n+1]
+    @cache
+    def get_rhom(self, n):
+        # construct Group hom, G0 --> G1
+        G0 = self.get_G(n)
+        G1 = self.get_G(n+1)
         items = G1.items
 
         # add new item on the right
@@ -181,6 +156,14 @@ def test_weyl():
         #print("hom:", len(hom))
         check_group_hom(G0, G1, hom)
         rhoms[n] = hom
+        return hom
+
+    @cache
+    def get_lhom(self, n):
+        # construct Group hom, G0 --> G1
+        G0 = self.get_G(n)
+        G1 = self.get_G(n+1)
+        items = G1.items
 
         # add new item on the left
         lookup = {src : letters[letters.index(src)+1] for src in G0.items}
@@ -195,69 +178,125 @@ def test_weyl():
             hom[g] = h
         check_group_hom(G0, G1, hom)
         #print("hom:", len(hom))
-        lhoms[n] = hom
+        #lhoms[n] = hom
+        return hom
 
-    def check_injection(src, tgt, func):
-        assert set(func.keys()) == set(src)
-        output = set(func.values())
-        assert len(output) == len(func)
-        assert output.issubset(tgt)
-
-    lfuncs = {}
-    rfuncs = {}
-    for n in range(N-1):
-      for m in range(n+1):
+    @cache
+    def get_rfunc(self, n, m):
+        assert 0<=n
+        assert 0<=m<=n
         #print("\n%d choose %d" % (n, m))
 
         # construct a right map
-        src = Xs[n, m]
-        tgt = Xs[n+1, m+1]
+        src = self.get_X(n, m)
+        tgt = self.get_X(n+1, m+1)
         #print(len(src.items), ">-->", len(tgt.items))
         func = {} # map src --> tgt
         for x in src.items:
             # find a coset representative
             g = src.repr[x]
             assert src[g](src.basepoint) == x
-            h = lhoms[n][g]
+            h = self.get_lhom(n)[g]
             y = tgt[h](tgt.basepoint)
             func[x] = y
         check_injection(src.items, tgt.items, func)
-        rfuncs[n, m] = func
+        #rfuncs[n, m] = func
+        return func
 
+    @cache
+    def get_lfunc(self, n, m):
+        assert 0<=n
+        assert 0<=m<=n
         # construct a left map
-        src = Xs[n, m]
-        Y = Ys[n+1, m]
-        tgt = Xs[n+1, m]
+        src = self.get_X(n, m)
+        Y = self.get_Y(n+1, m)
+        tgt = self.get_X(n+1, m)
         func = {} # map src --> tgt
         for x in src.items:
             # find a coset representative
             g = src.repr[x]
             assert src[g](src.basepoint) == x
-            h = lhoms[n][g]
+            h = self.get_lhom(n)[g]
             y = Y[h](Y.basepoint)
-            y = isos[n+1,m][y] # send Y --> tgt
+            y = self.get_iso(n+1,m)[y] # send Y --> tgt
             func[x] = y
         check_injection(src.items, tgt.items, func)
-        lfuncs[n, m] = func
+        #lfuncs[n, m] = func
+        return func
 
-        hom = {}
 
+class PascalA(Pascal):
+
+    @cache
+    def get_G(self, n):
+        letters = ascii_lowercase[:n]
+        items = list(letters)
+    
+        gen = []
+        for i in range(n-1):
+            perm = {c:c for c in items}
+            perm[items[i]] = items[i+1]
+            perm[items[i+1]] = items[i]
+            gen.append(Perm(perm, items))
+        G = Group.generate(gen, items=items)
+        return G
+    
+    
+class PascalB(Pascal):
+
+    @cache
+    def get_G(self, n):
+        letters = ascii_lowercase[:n]
+        items = ["+"+c for c in letters] + ["-"+c for c in letters]
+        pairs = [("+"+c, "-"+c) for c in letters]
+    
+        gen = []
+        for i in range(n-1):
+            perm = {c:c for c in items}
+            a, b = pairs[i]
+            c, d = pairs[i+1]
+            perm[a] = c
+            perm[c] = a
+            perm[b] = d
+            perm[d] = b
+            gen.append(Perm(perm, items))
+        if n:
+            perm = {c:c for c in items}
+            a, b = pairs[-1]
+            perm[a] = b
+            perm[b] = a
+            gen.append(Perm(perm, items))
+        G = Group.generate(gen, items=items)
+        return G
+
+
+def test_weyl():
+
+    print("test_weyl:")
+
+    triangle = PascalA()
+    assert triangle.get_G(3) is triangle.get_G(3)
+    assert triangle.get_G(3) is not PascalA().get_G(3)
+
+    N = 6
     n, m = 4, 2
     for n in range(2, N):
       for m in range(1, n):
-        rmap = rfuncs[n-1, m-1]
-        lmap = lfuncs[n-1, m]
+        rfunc = triangle.get_rfunc(n-1, m-1)
+        lfunc = triangle.get_lfunc(n-1, m)
     
-        tgt = Xs[n, m]
+        tgt = triangle.get_X(n, m)
         found = set(tgt.items)
-        for item in rmap.values():
+        for item in rfunc.values():
             assert item in found
             found.remove(item)
     
-        for item in lmap.values():
+        for item in lfunc.values():
             assert item in found
             found.remove(item)
         assert not found
+        print("(%d %d)"%(n,m), end=" ")
+      print()
 
 
 
@@ -271,7 +310,7 @@ if __name__ == "__main__":
         fn = eval(fn)
         fn()
 
-    print("finished in %.3f seconds.\n"%(time() - start_time))
+    print("OK: finished in %.3f seconds.\n"%(time() - start_time))
 
 
 
