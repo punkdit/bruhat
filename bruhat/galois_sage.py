00f1be0c77720f70af2eee6f2d29ce80f27efdcf
Simon Burton <simon@arrowtheory.com>
2023-04-24 10:51:14 +0100

    commit edits

--- bruhat/galois_sage.py
+++ bruhat/galois_sage.py
@@ -8,11 +8,15 @@ from time import time
 start_time = time()
 import string
 
-from bruhat.argv import argv
-from bruhat.smap import SMap
-
 from sage.all_cmdline import *
 from sage import all_cmdline 
+
+from bruhat.argv import argv
+from bruhat.smap import SMap
+from huygens.namespace import *
+
+yellow = RGB(1.0, 0.8, 0.2)
+orange = RGB(252/255, 132/255, 3/255) * (1./0.8)
 
 
 def main():
@@ -28,9 +32,48 @@ def main():
         #build(44, 2, 10)
         build(63, 6, 6)
 
+class Table(object):
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.values = {}
+    def __setitem__(self, key, value):
+        i, j = key
+        self.values[key] = value
+    def __getitem__(self, key):
+        return self.values[key]
+    @property
+    def smap(self):
+        s = SMap()
+        rows, cols = self.rows, self.cols
+        for row in range(rows):
+          for col in range(cols):
+            value = self.values.get((row, col), ".")
+            s[row, col] = value
+        return s
+    def __str__(self):
+        s = self.smap
+        return str(s)
+    def render(self):
+        cvs = Canvas()
+        dx, dy = 1., 1.
+        m, n = (self.rows), (self.cols)
+        values = self.values
+        for i in range(m):
+         for j in range(n):
+            k = values.get((i, j))
+            cvs.fill(path.rect(j*dx, -i*dy, dx, -dy), [white if k is None else orange])
+        st = st_THick
+        for i in range(m+1):
+            cvs.stroke(path.line(0, -i*dy, n*dx, -i*dy), st)
+        for i in range(n+1):
+            cvs.stroke(path.line(i*dx, 0, i*dx, -m*dy), st)
+        return cvs
 
-def build(n, rows, cols):
-    K = CyclotomicField(n)
+
+
+def build(nn, nrows, ncols):
+    K = CyclotomicField(nn)
     G = K.galois_group()
     Hs = G.subgroups()
     for H in Hs:
@@ -43,46 +86,79 @@ def build(n, rows, cols):
     items = []
     for H in Hs:
         gens = H.gens()
-        s = SMap()
-        for row in range(rows):
-             for col in range(cols):
-                s[row, col] = '.'
+        table = Table(nrows, ncols)
         for g in H:
           cs = list(g.cycles()) or [(1,)]
           c = cs[0]
           c = eval(str(c)) # wtf
           for ii, i in enumerate(c):
               i -= 1 # one-based index
-              col = i%cols
-              row = i//cols
+              col = i%ncols
+              row = i//ncols
               if ii in [0,1]:
-                s[row, col] = "X"
-        print(s)
+                table[row, col] = "X"
+        print(table)
         K0 = H.fixed_field()[0]
         print(K0.defining_polynomial())
         print(H.fixed_field())
         print()
-        item = (H, s, K0.defining_polynomial())
+        item = (H, table, K0.defining_polynomial())
         items.append(item)
 
     N = 1
     col = 0
     smap = SMap()
+    rows = []
+    row = []
     for item in items:
         if len(item[0]) == N:
-            smap.paste(item[1], 0, col)
+            smap.paste(item[1].smap, 0, col)
         else:
-            print(smap, rows*cols//N, '\n')
+            rows.append(row)
+            row = []
+            print(smap, nrows*ncols//N, '\n')
             N = len(item[0])
             smap = SMap()
             col = 0
-            smap.paste(item[1], 0, col)
-        col += cols+1
-    print(smap, rows*cols//N, '\n')
+            smap.paste(item[1].smap, 0, col)
+        row.append(item)
+        col += ncols+1
+    rows.append(row)
+    print(smap, nrows*ncols//N, '\n')
+
+    fg = Canvas()
+    x = 0
+    y = 0
+    margin = 0.2
+    get_width = lambda bb : 1.2*bb.width + margin
+    lookup = {} # map index to cvs
+    for row in rows:
+        cvss = []
+        for item in row:
+            cvs = item[1].render()
+            idx = Hs.index(item[0])
+            lookup[idx] = cvs
+            cvss.append(cvs)
+        width = sum(get_width(cvs.get_bound_box()) for cvs in cvss)
+        x = -0.5*width
+        for cvs in cvss:
+            bb = cvs.get_bound_box()
+            fg.insert(x, y, cvs)
+            x += get_width(bb)
+        y -= 2.2*bb.height + margin
+    bg = Canvas()
+    links = get_links(items)
+    st = st_THICk + [grey]
+    for (i, j) in links:
+        src = fg.find_bound_box(lookup[i]).south
+        tgt = fg.find_bound_box(lookup[j]).north
+        bg.stroke(path.line(*src, *tgt), st)
+    cvs = Canvas([bg, fg])
+    cvs.writePDFfile("galois_%s.pdf"%nn)
 
 
-def make_dot(items):
-    # not great....
+
+def get_links(items):
     n = len(items)
     links = set()
     for i in range(n):
@@ -96,7 +172,12 @@ def make_dot(items):
       for k in range(j+1, n):
         if (i,j) in links and (j,k) in links and (i,k) in links:
             links.remove((i,k))
+    return links
 
+def make_dot(items):
+    # not great....
+    n = len(items)
+    links = get_links(items)
     letters = string.ascii_letters
     lines = ["digraph {"]
     for (i,j) in links:
