b504e12847a8339f704f574fb35f3d2eb51f1df6
Simon Burton <simon@arrowtheory.com>
2025-11-23 18:04:10 +0000

    commit edits

--- bruhat/lax.py
+++ bruhat/lax.py
@@ -17,9 +17,11 @@ from sage import all_cmdline as sage
 
 
 from bruhat.matrix_sage import Matrix as SMatrix
-from bruhat.algebraic import Algebraic
-from bruhat.action import mulclose
+from bruhat.algebraic import Algebraic, Matrix
+from bruhat.gset import mulclose, Perm, Group
 from bruhat.argv import argv
+from bruhat import solve as lin
+from bruhat.util import cross
 
 p = argv.get("p", 2)
 
@@ -280,12 +282,12 @@ def test_qpoly():
         (3, ((q-1)*(q)*(q+1))**2 ),
         (4, (q-1)**2 * q * (q+1)**4),
         (1, (q+1)**4 * q * (q-1)**3),
-        #(1, (q-2)*(q+1)**4 * q**3 * (q-1)**1),
-        (1, (q - 2) * (q - 1)**3 * q**3 * (q + 1)**3),
+        ((q - 2), (q - 1)**3 * q**3 * (q + 1)**3),
         (3, (q+1)**4 * q**2 * (q-1)**3),
     ]
     for m,item in items:
         v = v + m*item
+    assert v == f
 
     for p in [2,3,5,7]:
         print("p = %s"%p)
@@ -296,15 +298,209 @@ def test_qpoly():
     #print(v)
     #print(factor(f-v))
 
-    poly = (q - 2) * (q - 1)**3 * q**3 * (q + 1)**3
-    #print( poly )
-    #print( poly / (q-2) )
-
-    poly = (q - 1)**3 * q**3 * (q + 1)**3
-    for p in [2,3,5,7]:
-        print("p=%d"%p, poly.subs(q=p))
 
 
+def lc(M, i):
+    #print("lc", i)
+    n = len(M)
+    nbd = [j for j in range(n) if M[i,j]]
+    #print("nbd:", nbd)
+
+    B = M[:, nbd][nbd, :]
+    #print(B)
+    B = Matrix(B.A + 1)
+    #print(B)
+
+    A = M.A.copy()
+    for i in nbd:
+      for j in nbd:
+        if i!=j:
+            A[i,j] = 1-A[i,j]
+    M = Matrix(A)
+    return M
+    
+
+
+def graph_lc():
+
+    n = argv.get("n", 4)
+    p = n*(n-1)//2
+    count = 2**p
+
+    items = set()
+    idxs = [(i,j) for i in range(n) for j in range(i+1,n)]
+    #print(idxs)
+    for bits in numpy.ndindex((2,)*p):
+        A = numpy.zeros((n,n), dtype=int)
+        #print(bits)
+        for (i,j),bit in zip(idxs, bits):
+            A[i,j] = bit
+            A[j,i] = bit
+        M = Matrix(A)
+        items.add(M)
+        #print(M)
+    #print()
+
+    assert len(items) == count
+
+    items = list(items)
+    items.sort(key=str)
+
+    remain = set(items)
+    for M in remain:
+        for i in range(n):
+            M1 = lc(M,i)
+            #print(M1)
+            assert M1 in remain
+
+    orbits = []
+    while remain:
+        M = remain.pop()
+        found = {M}
+        bdy = [M]
+        while bdy:
+            _bdy = []
+            for M in bdy:
+              for i in range(n):
+                M1 = lc(M, i)
+                assert lc(M1, i) == M
+                if M1 in remain:
+                    found.add(M1)
+                    _bdy.append(M1)
+                    remain.remove(M1)
+            bdy = _bdy
+        #print(len(found))
+        orbits.append(list(found))
+    orbits.sort(key = len)
+    print([len(o) for o in orbits])
+
+    gens = []
+    lookup = {M:i for (i,M) in enumerate(items)}
+    for i in range(n):
+        perm = [lookup[lc(M,i)] for M in items]
+        perm = Perm(perm)
+        gens.append(perm)
+        #print(perm)
+
+    #G = mulclose(gens, verbose=True)
+    #print(len(G))
+
+    G = Group(gens = gens, build=False)
+    #print(G.structure_description())
+
+    #f = open("lc_graphs.gap", "w")
+    #print("G := %s;"%G.gapstr(), file=f)
+    #f.close()
+
+
+def quadratic_form():
+
+    n = argv.get("n", 2)
+    q = argv.get("q", 3)
+    p = n*(n+1)//2
+
+    print(q**p)
+
+    R = sage.GF(q)
+
+    items = set()
+    idxs = [(i,j) for i in range(n) for j in range(i,n)]
+    assert len(idxs) == p
+    #print(idxs)
+    for bits in numpy.ndindex((q,)*p):
+        A = numpy.zeros((n,n), dtype=int)
+        #print(bits)
+        for (i,j),bit in zip(idxs, bits):
+            A[i,j] = bit
+            A[j,i] = bit
+        Q = SMatrix(R, A)
+        items.add(Q)
+        #print(Q)
+    #print()
+
+    print(len(items))
+
+    G = sage.GL(n, R)
+    gens = []
+    for g in G.gens():
+        M = SMatrix(R, g)
+        gens.append(M)
+
+    #G = mulclose(gens)
+    #print(len(G))
+
+    orbits = []
+    remain = set(items)
+    while remain:
+        Q = remain.pop()
+        orbit = [Q]
+        bdy = [Q]
+        while bdy:
+            _bdy = []
+            for Q in bdy:
+                for M in gens:
+                    R = M*Q*M.t
+                    if R not in remain:
+                        continue
+                    _bdy.append(R)
+                    remain.remove(R)
+            bdy = _bdy
+            orbit += bdy
+            #print(len(orbit), len(remain))
+        orbits.append(orbit)
+
+    orbits.sort(key=len)
+    counts = [len(orbit) for orbit in orbits]
+    print(counts, sum(counts))
+
+
+
+def search_poly():
+
+    R = sage.PolynomialRing(sage.ZZ, 'q')
+    q = R.gens()[0]
+
+    vals = argv.vals
+    vals = tuple(2*v for v in vals)
+    print("vals =", vals)
+    qs = [3, 5, 7, 11][:len(vals)]
+
+    lookup = {}
+    for degree in range(8):
+        #print("degree", degree)
+        #items = range(-5, 6)
+        items = [-1, 0, 1]
+        for cs in cross( [items] * (1+degree) ):
+            if cs[-1] == 0:
+                continue
+            p = sum( c*q**i for (i,c) in enumerate(cs) )
+            key = tuple(p.subs(q=val) for val in qs)
+            if key == vals:
+                print(p, "=", str(sage.factor(p)).replace(" ", ""))
+                return
+            #lookup[key] = p
+            #print(key, end=" ")
+        #print()
+    
+    #if vals in lookup:
+    #    print("found:", lookup[vals])
+    #else:
+    #    print("not found")
+
+
+def interpolate():
+
+    R = sage.PolynomialRing(sage.QQ, 'q')
+    q = R.gens()[0]
+
+    vals = argv.vals
+    print("vals =", vals)
+    qs = [3, 5, 7, 11, 13][:len(vals)]
+
+    points = list(zip(qs, vals))
+    print(points)
+    p = R.lagrange_polynomial(points)
+    print(p, "=", sage.factor(p))
 
 
 
